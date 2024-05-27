c7699a2e23d40c9bb9337d39e2f4c908719a69fd
Naim <naimxfarhat21@gmail.com>
2024-05-27 12:42:13 +0800

    add global state using atom

---
+++ src/app/atom.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+import { atom } from "jotai";
+
+export const countryAtom = atom("MY");
+
+export const placeAtom = atom("Kuala Lumpur");
+
+export const loadingCityAtom = atom(false);
